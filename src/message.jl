c69633e2b02467ee59cb5395c5e732c7639c19d2
wildart <wildart@gmail.com>
2015-09-06 03:57:08 -0400

    added tests for Messages

--- src/message.jl
+++ src/message.jl
@@ -3,6 +3,7 @@ module Messages
     using Log4jl: Message
     import Log4jl: format, formatted, parameters
 
+    "Message handles everything as string."
     type SimpleMessage <: Message
         message::AbstractString
     end
@@ -11,20 +12,24 @@ module Messages
     parameters(msg::SimpleMessage) = nothing
     Base.show(io::IO, msg::SimpleMessage) = print(io, "SimpleMessage[message=",msg.message,']')
 
+
+    "Message with raw objects"
     type ObjectMessage <: Message
         message::Any
     end
     formatted(msg::ObjectMessage)  = string(msg.message)
     format(msg::ObjectMessage)     = formatted(msg)
-    parameters(msg::ObjectMessage) = [msg.message]
+    parameters(msg::ObjectMessage) = Any[msg.message]
     Base.show(io::IO, msg::ObjectMessage) = print(io, "ObjectMessage[message=",msg.message,']')
 
-    """ Message pattern contains placeholders indicated by '{}' """
-    type StringFormattedMessage <: Message
+
+    "Message pattern contains placeholders indicated by '{}'"
+    type ParameterizedMessage <: Message
         pattern::AbstractString
         params::Vector{Any}
     end
-    function formatted(msg::StringFormattedMessage)
+    ParameterizedMessage(ptrn::AbstractString, params...) = ParameterizedMessage(ptrn, [params...])
+    function formatted(msg::ParameterizedMessage)
         offs = map(ss->ss.offset, matchall(r"({})+",msg.pattern))
         @assert length(offs) == length(msg.params) "Pattern does not match parameters"
         sstart = 1
@@ -36,16 +41,18 @@ module Messages
         end
         sformatted
     end
-    format(msg::StringFormattedMessage)     = msg.pattern
-    parameters(msg::StringFormattedMessage) = msg.params
-    Base.show(io::IO, msg::StringFormattedMessage) =
-        print(io, "StringFormatMessage[pattern='",msg.pattern,"', args=",msg.params,']')
+    format(msg::ParameterizedMessage)     = msg.pattern
+    parameters(msg::ParameterizedMessage) = msg.params
+    Base.show(io::IO, msg::ParameterizedMessage) =
+        print(io, "ParameterizedMessage[pattern='",msg.pattern,"', args=",msg.params,']')
 
-    """ Message pattern contains 'printf' format string"""
+
+    "Message pattern contains 'printf' format string"
     type PrintfFormattedMessage <: Message
         pattern::AbstractString
         params::Vector{Any}
     end
+    PrintfFormattedMessage(ptrn::AbstractString, params...) = PrintfFormattedMessage(ptrn, [params...])
     formatted(msg::PrintfFormattedMessage)  = @eval @sprintf($(msg.pattern), $(msg.params)...)
     format(msg::PrintfFormattedMessage)     = msg.pattern
     parameters(msg::PrintfFormattedMessage) = msg.params
@@ -53,17 +60,20 @@ module Messages
         print(io, "PrintfFormattedMessage[pattern='",msg.pattern,"', args=",msg.params,']')
 
 
+    #TODO: MapMessage: XML, JSON, Dict
+    #TODO: StructuredDataMessage: RFC 5424
+
     # Message generating functions
     simplemessage(msg::AbstractString, params...) = SimpleMessage(msg)
     simplemessage(msg::Any) = ObjectMessage(msg)
 
-    stringsformattermessage(msg::AbstractString, params...) = StringFormattedMessage(msg, [params...])
-    stringsformattermessage(msg::Any) = ObjectMessage(msg)
+    parameterizedmessage(msg::AbstractString, params...) = StringFormattedMessage(msg, [params...])
+    parameterizedmessage(msg::Any) = ObjectMessage(msg)
 
     printfformattermessage(msg::AbstractString, params...) = PrintfFormattedMessage(msg, [params...])
     printfformattermessage(msg::Any) = ObjectMessage(msg)
 
 
-    export simplemessage, stringsformattermessage, printfformattermessage
+    export simplemessage, parameterizedmessage, printfformattermessage
 
 end
---
+++ test/message.jl
@@ -0,0 +1,48 @@
+module TextMessages
+
+using Log4jl
+using Log4jl.Messages
+import Log4jl.Messages: SimpleMessage, ObjectMessage, ParameterizedMessage, PrintfFormattedMessage
+using FactCheck
+
+type IncompleteMessage <: Log4jl.Message
+end
+
+msgInput = ["test", 10, 11.]
+
+facts("Massages") do
+    context("should have required methods implemented") do
+        msg = IncompleteMessage()
+        @fact_throws AssertionError format(msg)
+        @fact_throws AssertionError parameters(msg)
+        @fact_throws AssertionError formatted(msg)
+    end
+    context("can be created from string input") do
+        msg = SimpleMessage(msgInput[1])
+        @fact format(msg) --> msgInput[1]
+        @fact parameters(msg) --> nothing
+        @fact formatted(msg) --> msgInput[1]
+    end
+    context("can be created from object input") do
+        msg = ObjectMessage(msgInput)
+        @fact format(msg) --> string(msgInput)
+        @fact parameters(msg) --> Any[msgInput]
+        @fact formatted(msg) --> string(msgInput)
+    end
+    context("can be created from parameterized string input") do
+        msgPattern = "Test: {}, {}, {}"
+        msg = ParameterizedMessage(msgPattern, msgInput)
+        @fact format(msg) --> msgPattern
+        @fact parameters(msg) --> msgInput
+        @fact formatted(msg) --> "Test: test, 10, 11.0"
+    end
+    context("can be created from 'printf' formatted input") do
+        msgPattern = "Test: %s, %d, %.1f"
+        msg = PrintfFormattedMessage(msgPattern, msgInput)
+        @fact format(msg) --> msgPattern
+        @fact parameters(msg) --> msgInput
+        @fact formatted(msg) --> "Test: test, 10, 11.0"
+    end
+end
+
+end
