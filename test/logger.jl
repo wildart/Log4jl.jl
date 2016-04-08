d14809393d47e4428b56a475a6b1cae4f29e0d77
wildart <wildart@gmail.com>
2016-04-08 13:47:19 -0400

    fixed failed tests after context selector changes

--- test/logger.jl
+++ test/logger.jl
@@ -57,7 +57,7 @@ facts("Macro `logger` accepts") do
         res = @logger begin 1 end
         @fact res[1] --> cmname
         @fact res[2] --> Log4jl.LOG4JL_DEFAULT_MESSAGE
-        @fact isnull(res[3]) --> false
+        @fact isnull(res[3]) --> true
         @fact isnull(res[4]) --> false
     end
     context("logger name and massage factory") do
@@ -78,7 +78,7 @@ facts("Macro `logger` accepts") do
         res = @logger "AAA" begin 1 end
         @fact res[1] --> "AAA"
         @fact res[2] --> Log4jl.LOG4JL_DEFAULT_MESSAGE
-        @fact isnull(res[3]) --> false
+        @fact isnull(res[3]) --> true
         @fact isnull(res[4]) --> false
     end
     context("massage factory and configuration uri") do
@@ -99,14 +99,14 @@ facts("Macro `logger` accepts") do
         res = @logger MSG=Log4jl.LOG4JL_DEFAULT_STATUS_LEVEL begin 1 end
         @fact res[1] --> cmname
         @fact res[2] --> Log4jl.LOG4JL_DEFAULT_STATUS_LEVEL
-        @fact isnull(res[3]) --> false
+        @fact isnull(res[3]) --> true
         @fact isnull(res[4]) --> false
     end
     context("name, massage factory and programmable configuration") do
         res = @logger "AAA" MSG=Log4jl.LOG4JL_DEFAULT_STATUS_LEVEL begin 1 end
         @fact res[1] --> "AAA"
         @fact res[2] --> Log4jl.LOG4JL_DEFAULT_STATUS_LEVEL
-        @fact isnull(res[3]) --> false
+        @fact isnull(res[3]) --> true
         @fact isnull(res[4]) --> false
     end
     context("name, configuration uri and massage factory") do
